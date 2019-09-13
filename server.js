2f998bc484b3312655d858c42585f33ae6288570
Thiago Luis Vasconcellos Behm <thiagoluisbehm@gmail.com>
2019-09-13 15:51:03 -0300

    added multer as the parser

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const multer = require('multer')
 const request = require('request');
 const mail = require('./mail.js');
 
@@ -9,29 +9,17 @@ const port = process.env.PORT || 3000;
 //starts express
 let app = express();
 
+var upload = multer();
+
 //parses the body parameters
 let urlencodedParser = bodyParser.urlencoded({
   extended: false
 });
 
 
-app.post('/webhook', urlencodedParser (req, resp) => {
-	mail.transporter.verify().then(function(con) {
-      
+app.post('/webhook', upload.none(), (req, resp, next) => {
 
-      if (result.isEmpty()) {
-        mail.enviarEmail(req.body);
-        session.success = true;
-        session.data = '';
-        session.erros = '';
-      } else {
-        session.success = false;
-        session.erros = result.array();
-        session.data = req.body;
-      }
-    }).catch(function(con) {
-    	console.log("Error while submiting the email");
-    });
+  console.log(req.body);
 
 });
 
